25827fb6fc78381ffb93f80f625de05a220de92a
22jy0126 <22jy0126@jec.ac.jp>
2023-12-12 15:58:05 +0900

    初期化

---
+++ src/test/java/com/hikariSchool/CourseManagement1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikariSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagement1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
